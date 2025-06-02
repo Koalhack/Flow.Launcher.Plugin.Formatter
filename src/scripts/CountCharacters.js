c95de5fd119fcfe0356ababd7339cfb58844989b
Koalhack <33071384+Koalhack@users.noreply.github.com>
2025-06-02 18:56:41 +0200

    feat(builtin): Added `CountCharacters.js` script

---
+++ src/scripts/CountCharacters.js
@@ -0,0 +1,14 @@
+/**
+	{
+		"api":1,
+		"name":"Count Characters",
+		"description":"Get the length of your text",
+		"author":"Ivan",
+		"icon":"counter",
+		"tags":"count,length,size,character"
+	}
+**/
+
+function main(input) {
+  input.postInfo(`${input.text.length} characters`);
+}
